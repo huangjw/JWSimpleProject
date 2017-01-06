c3882d776bd422605418d123012467907eb48854
huangjw <jiawei.huang@midea.com>
2017-01-06 14:32:53 +0800

    Initial Commit

---
+++ JWSimpleProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JWSimpleProject
+//
+//  Created by huangjw on 2017/1/6.
+//  Copyright © 2017年 huangjw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
